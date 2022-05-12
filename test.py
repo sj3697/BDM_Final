5cb83d6933d6ac47926e507c549ca01aa1330f29
59418 <90524952+sj3697@users.noreply.github.com>
2022-05-12 11:27:07 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -8,12 +8,14 @@ import pandas as pd
 
 def main(sc):
     spark = SparkSession(sc)
+    final = spark.read.load('nyc_cbg_centroids.csv', format='csv', header=True, inferSchema=True)
+    final = final.select(final['cbg_fips'].alias('cbg'))
+    
     df = pd.read_csv('nyc_cbg_centroids.csv')
     outputCBG = df.set_index('cbg_fips').T.to_dict('list')
 
     #CBG = 'nyc_cbg_centroids.csv'
-    final = spark.read.load('nyc_cbg_centroids.csv', format='csv', header=True, inferSchema=True)
-    final = final.select(final['cbg_fips'].alias('cbg'))
+    
     final.saveAsTextFile('test')
 
 if __name__ == '__main__':
