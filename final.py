43e505786f6569cbb46843c4c16fb1ce4b67a613
59418 <90524952+sj3697@users.noreply.github.com>
2022-05-12 10:50:12 -0700

    Add files via upload

--- final.py
+++ final.py
@@ -89,7 +89,7 @@ def readPatterns_2020_10(partId, part):
 
 
 def main(sc):
-    spark = SparkSession()
+    spark = SparkSession(sc)
     df = pd.read_csv('nyc_cbg_centroids.csv')
     outputCBG = df.set_index('cbg_fips').T.to_dict('list')
 
