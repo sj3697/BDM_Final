e5d0e3e84983319d0aa84398b7f14133ae00cd5b
59418 <90524952+sj3697@users.noreply.github.com>
2022-05-14 12:41:02 -0700

    Add files via upload

--- final.py
+++ final.py
@@ -1,158 +1,61 @@
 import csv
+import sys
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
+from pyspark.sql import *
+from pyspark.sql import functions as f
 from pyspark.sql import types as T
 from pyspark import SparkContext
+from pyspark.sql import Window
+import pandas as pd
+import ast
 
-#!pip install pyproj
 from pyproj import Transformer
 import shapely
 from shapely.geometry import Point
-import pandas as pd
-
-
-def readPlacekey(partId, part):
-  if partId == 0: next(part)
-  for x in csv.reader(part):
-    yield x[9]
-
-
-def readPatterns(partId, part):
-  t = Transformer.from_crs(4326, 2263)
-  if partId == 0: next(part)
-  for x in csv.reader(part):
-    if x[0] in outputSuermarket:
-      if '2019-04-01' > x[12] >= '2019-03-01' or '2019-04-01' > x[13] >= '2019-03-01':
-        temp = ast.literal_eval(x[19])
-        id = int(x[18])
-        for key in temp:
-          k = int(key)
-          if k in outputCBG:
-            a = t.transform(outputCBG[k][0],outputCBG[k][1])
-            b = t.transform(outputCBG[id][0],outputCBG[id][1])
-            dis = Point(a).distance(Point((b)))/5280
-            yield k, dis*temp[key], temp[key]
 
 
 
-def readPatterns_2019_10(partId, part):
-  t = Transformer.from_crs(4326, 2263)
-  if partId == 0: next(part)
-  for x in csv.reader(part):
-    if x[0] in outputSuermarket:
-      if '2019-11-01' > x[12] >= '2019-10-01' or '2019-11-01' > x[13] >= '2019-10-01':
-        temp = ast.literal_eval(x[19])
-        id = int(x[18])
-        for key in temp:
-          k = int(key)
-          if k in outputCBG:
-            a = t.transform(outputCBG[k][0],outputCBG[k][1])
-            b = t.transform(outputCBG[id][0],outputCBG[id][1])
-            dis = Point(a).distance(Point((b)))/5280
-            yield k, dis*temp[key], temp[key]
-
-
-def readPatterns_2020_03(partId, part):
-  t = Transformer.from_crs(4326, 2263)
-  if partId == 0: next(part)
-  for x in csv.reader(part):
-    if x[0] in outputSuermarket:
-      if '2020-04-01' > x[12] >= '2020-03-01' or '2020-04-01' > x[13] >= '2020-03-01':
-        temp = ast.literal_eval(x[19])
-        id = int(x[18])
-        for key in temp:
-          k = int(key)
-          if k in outputCBG:
-            a = t.transform(outputCBG[k][0],outputCBG[k][1])
-            b = t.transform(outputCBG[id][0],outputCBG[id][1])
-            dis = Point(a).distance(Point((b)))/5280
-            yield k, dis*temp[key], temp[key]
-
-
-
-def readPatterns_2020_10(partId, part):
-  t = Transformer.from_crs(4326, 2263)
-  if partId == 0: next(part)
-  for x in csv.reader(part):
-    if x[0] in outputSuermarket:
-      if '2020-11-01' > x[12] >= '2020-10-01' or '2020-11-01' > x[13] >= '2020-10-01':
-        temp = ast.literal_eval(x[19])
-        id = int(x[18])
-        for key in temp:
-          k = int(key)
-          if k in outputCBG:
-            a = t.transform(outputCBG[k][0],outputCBG[k][1])
-            b = t.transform(outputCBG[id][0],outputCBG[id][1])
-            dis = Point(a).distance(Point((b)))/5280
-            yield k, dis*temp[key], temp[key]
-
-
-def main(sc):
-    spark = SparkSession(sc)
+def main(sc,sqlcontext):
     df = pd.read_csv('nyc_cbg_centroids.csv')
     outputCBG = df.set_index('cbg_fips').T.to_dict('list')
-
-    #CBG = 'nyc_cbg_centroids.csv'
-    final = spark.read.load('nyc_cbg_centroids.csv', format='csv', header=True, inferSchema=True)
-    final = final.select(final['cbg_fips'].alias('cbg'))
     
-    outputSuermarket = sc.textFile('nyc_supermarkets.csv') \
-                .mapPartitionsWithIndex(readPlacekey).collect()
-    
-    # 2019_03
-    output2019_03 = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020') \
-              .mapPartitionsWithIndex(readPatterns)
-    #output2019_03.count()
-    deptColumns = ["cbg","dis","count"]
-    df_2019_03 = output2019_03.toDF(deptColumns)
-    df_2019_03 = df_2019_03.groupBy('cbg').sum('dis', 'count')
-    df_2019_03 = df_2019_03.withColumn('2019_03', (df_2019_03[1]/df_2019_03[2])).select('cbg', '2019_03')
-    final = final.join(df_2019_03, on = 'cbg', how = 'left')
-    #final.show()
 
-    #2019_10
-    output2019_10 = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020') \
-              .mapPartitionsWithIndex(readPatterns_2019_10)
-    #output2019_10.count()
+    df_s = pd.read_csv('nyc_supermarkets.csv')
+    outputSupermarket = df_s['safegraph_placekey'].to_numpy()
 
-    deptColumns = ["cbg","dis","count"]
-    df_2019_10 = output2019_10.toDF(deptColumns)
-    df_2019_10 = df_2019_10.groupBy('cbg').sum('dis', 'count')
-    df_2019_10 = df_2019_10.withColumn('2019_10', (df_2019_10[1]/df_2019_10[2])) \
-        .select('cbg', '2019_10')
-    final = final.join(df_2019_10, on = "cbg", how='left')
-    #final.show()
+    def readPatterns_2(partId, part):
+      t = Transformer.from_crs(4326, 2263)
+      if partId == 0: next(part)
+      for x in csv.reader(part):
+        if x[0] in outputSupermarket:
+          if '2019-04-01' > x[12] >= '2019-03-01' or '2019-04-01' > x[13] >= '2019-03-01':
+            temp = ast.literal_eval(x[19])
+            id = int(x[18])
+            for key in temp:
+              k = int(key)
+              if k in outputCBG:
+                a = t.transform(outputCBG[k][0],outputCBG[k][1])
+                b = t.transform(outputCBG[id][0],outputCBG[id][1])
+                dis = Point(a).distance(Point((b)))/5280
+                for i in range(temp[key]):
+                  yield k, dis*temp[key]
+                  
+      output2_2019_03 = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020') \
+              .mapPartitionsWithIndex(readPatterns_2)
+      
+##      deptColumns = ["cbg","dis"]
+##      df2_2019_03 = output2_2019_03.toDF(deptColumns)
+##      med = f.expr('percentile_approx(dis,0.5)')
+##      df2_2019_03 = df2_2019_03.groupBy('cbg').agg(med.alias('2019_03'))
 
-    #2020_03
-    output2020_03 = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020') \
-              .mapPartitionsWithIndex(readPatterns_2020_03)
-    #output2020_03.count()
-
-    deptColumns = ["cbg","dis","count"]
-    df_2020_03 = output2020_03.toDF(deptColumns)
-    df_2020_03 = df_2020_03.groupBy('cbg').sum('dis', 'count')
-    df_2020_03 = df_2020_03.withColumn('2020_03', (df_2020_03[1]/df_2020_03[2])) \
-        .select('cbg', '2020_03')
-    final = final.join(df_2020_03, on = "cbg", how='left').cache()
-    #final.show()
-
-    #2020_10
-    output2020_10 = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020') \
-              .mapPartitionsWithIndex(readPatterns_2020_10)
-    #output2020_10.count()
-
-    deptColumns = ["cbg","dis","count"]
-    df_2020_10 = output2020_10.toDF(deptColumns)
-    df_2020_10 = df_2020_10.groupBy('cbg').sum('dis', 'count')
-    df_2020_10 = df_2020_10.withColumn('2020_10', (df_2020_10[1]/df_2020_10[2])) \
-        .select('cbg', '2020_10')
-    final = final.join(df_2020_10, on = "cbg", how='left').cache()
-    #final.show()
-
-    final = final.orderBy('cbg', ascending=True)
-    final.saveAsTextFile('Final')
-    #final.show()
+      
+      #df2_2019_03.write.option("header", True).csv(sys.argv[1])
+      output2_2019_03.saveAsTextFile('extra')
+      
 if __name__ == '__main__':
   sc = SparkContext()
-  main(sc)
+  sqlcontext=SQLContext(sc)
+  main(sc,sqlcontext)
+
+
